1dcecfb15687e82dbaf0c059b82b42b1e718c512
RedTerror <37833085+RedTerrorDark@users.noreply.github.com>
2025-09-23 18:31:58 +0700

    EtotCommitOtkataProsit

--- main.cpp
+++ main.cpp
@@ -11,13 +11,18 @@ using namespace std;
 int main()
 {
 
-  double u = 2.4 * pow(10, -4), p = 0.721 * pow(10, 4), l = 15.4;
-  double R0 = 0.0816, R1 = R0, a = R0, R2 = 0.0264;
-  double Q1, Q2, Q3; /*Ответы*/
+  const double u = 0.00024,
+               p = 7210,
+               l = 15.4;
   
-  Q1 = ((M_PI * pow(R0, 4)) / 8) * (p / (u * l));
-  Q2 = (M_PI / 8) * (p / (u * l)) * abs((pow(pow(R1, 2) - pow(R2, 2), 2) / (log(R2 / R1))) + pow(R1, 4) - pow(R2, 4));
-  Q3 = ((pow(a, 4) * pow(3, 0.5)) / 320) * (p / (u * l));
+  double R0 = 0.0816,
+         R1 = R0,
+         a = R0,
+         R2 = 0.0264;
+  
+ double Q1 = ((M_PI * pow(R0, 4)) / 8) * (p / (u * l));
+ double Q2 = (M_PI / 8) * (p / (u * l)) * abs((pow(pow(R1, 2) - pow(R2, 2), 2) / (log(R2 / R1))) + pow(R1, 4) - pow(R2, 4));
+ double Q3 = ((pow(a, 4) * pow(3, 0.5)) / 320) * (p / (u * l));
   
   cout<<"Q1 = " << Q1 << ", Q2 = " << Q2 << ", Q3 = " << Q3;
 
