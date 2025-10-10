6d6694f5d9ed1f28561167be3e495d8ba025edd0
RedTerror <37833085+RedTerrorDark@users.noreply.github.com>
2025-10-10 09:04:19 +0700

    CoolCommit

---
+++ laba5.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int mass( int numberOfArrayElements ){
+    
+  int *baseMassive = new int [numberOfArrayElements];
+  int minIndex = 9999*9999;
+  
+  for( int indexElement = 0; indexElement < numberOfArrayElements; ++indexElement) {
+      
+      cout << endl << "Введите элемент массива под номером (" << indexElement << ")";
+      cin >> baseMassive[indexElement];
+      
+      if (baseMassive[indexElement] < minIndex){
+          
+        minIndex = baseMassive[indexElement];
+        
+      }
+  }
+  
+  return(minIndex);
+
+}
+
+
+
+
+int main() {
+  
+  int X, Y, Z;
+  int k,n,m;
+  
+  cout << "Введите размер массива X: ";
+  cin >> k;
+  
+  X = mass(k);
+
+  cout << "Введите размер массива Y: ";
+  cin >> n;
+  
+  Y = mass(n);
+
+  cout << "Введите размер массива Z: ";
+  cin >> m;
+  
+  Z = mass(m);
+  
+  
+  cout << "Минимальный элемент массива X: " << X << "Минимальный элемент массива Y: " << Y << "Минимальный элемент массива Z: " << Z;
+
+  return(0);
+}
